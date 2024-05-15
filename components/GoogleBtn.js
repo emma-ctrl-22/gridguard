083fade7f3b5a0edb73bdb2dc168edafb5da9fc7
ellie-ctrl35 <146957037+ellie-ctrl35@users.noreply.github.com>
2024-05-15 12:16:16 +0100

    chore: Update Google login button styles and image source

--- components/GoogleBtn.js
+++ components/GoogleBtn.js
@@ -1,28 +1,28 @@
-import { StyleSheet, Text, View ,TouchableOpacity, Image} from 'react-native'
+import { StyleSheet, Text, View, TouchableOpacity, Image } from 'react-native'
 import React from 'react'
 
-export default function GoogleBtn({onPress,title}) {
-  return (
-    <TouchableOpacity style={styles.btn}>
-        <Image source={require('../assets/GoogleIcon.png')} style={styles.Glogo}/> 
-      <Text>{title}</Text>
-    </TouchableOpacity>
-  )
+export default function GoogleBtn({ onPress, title }) {
+    return (
+        <TouchableOpacity style={styles.btn} onPress={onPress}>
+            <Image source={require('../assets/GoogleIcon.png')} style={styles.Glogo} />
+            <Text>{title}</Text>
+        </TouchableOpacity>
+    )
 }
 
 const styles = StyleSheet.create({
-    btn:{
-        width:"82%",
-        height:50,
-        backgroundColor:"#FFFFFF",
-        justifyContent:"center",
-        alignItems:"center",
-        border:"#000",
-        borderRadius:5,
-        marginTop:"4%"
+    btn: {
+        width: "82%",
+        height: 50,
+        backgroundColor: "#FFFFFF",
+        justifyContent: "center",
+        alignItems: "center",
+        border: "#000",
+        borderRadius: 5,
+        marginTop: "4%"
     },
-    Glogo:{
-        width:25,
-        height:25,
+    Glogo: {
+        width: 25,
+        height: 25,
     }
 })
