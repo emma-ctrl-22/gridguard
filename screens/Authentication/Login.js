499f80e5d3eb331c49e9e0ec7a5ef5252bf71074
ellie-ctrl35 <146957037+ellie-ctrl35@users.noreply.github.com>
2024-05-15 04:09:32 +0100

    login screen header and logo

--- screens/Authentication/Login.js
+++ screens/Authentication/Login.js
@@ -1,12 +1,13 @@
 import { StyleSheet, SafeAreaView, Text, View, TouchableOpacity,  Image } from 'react-native';
 import React, { useState } from 'react';
-import Swiper from 'react-native-swiper';
-import slides from './slideItems';
+import Logo from '../../assets/GridGuard-Logo.png'
 
 const Login = () => {
   return (
     <SafeAreaView style={styles.container}>
-      
+      <Text style={styles.headertxt}>Sign in to your Account</Text>
+      <Text style={styles.smalltxt}>Enter your details to sign in to your account</Text>
+      <Image source={Logo} style={styles.logo}/>
     </SafeAreaView>
   );
 };
@@ -20,4 +21,17 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor:  '#fff', // Set background color based on color scheme
   },
+  headertxt:{
+    fontSize: 25,
+    fontWeight: 'bold',
+    color: '#000000' // Set text color based on color scheme
+  },
+  smalltxt:{
+    fontSize: 15,
+    color: '#000000', // Set text color based on color scheme
+    opacity:0.5
+  },
+  logo:{
+    marginTop:"6%"
+  }
 });
