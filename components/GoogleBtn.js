20780ef7cf89798dc008b764ae4aa1b39d46715b
ellie-ctrl35 <146957037+ellie-ctrl35@users.noreply.github.com>
2024-05-15 12:08:55 +0100

    feat: Add Google login button with custom styles

--- components/GoogleBtn.js
+++ components/GoogleBtn.js
@@ -1,12 +1,27 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View ,TouchableOpacity, Image} from 'react-native'
 import React from 'react'
 
 export default function GoogleBtn({onPress,title}) {
   return (
-    <View>
+    <TouchableOpacity style={styles.btn}>
+        <Image source={require('../assets/google.png')} style={styles.Glogo}/> 
       <Text>{title}</Text>
-    </View>
+    </TouchableOpacity>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    btn:{
+        width:"82%",
+        height:50,
+        backgroundColor:"#FFFFFF",
+        justifyContent:"center",
+        alignItems:"center",
+        border:"#000",
+        borderRadius:5,
+    },
+    Glogo:{
+        width:25,
+        height:25,
+    }
+})
