d8b8540cc5d7c834b644dde1ef8dc5bc568900aa
ellie-ctrl35 <146957037+ellie-ctrl35@users.noreply.github.com>
2024-05-20 16:24:48 +0100

    refactor: Update OnBoard screen styling and add BigLogo component

--- screens/Authentication/OnBoard.js
+++ screens/Authentication/OnBoard.js
@@ -1,23 +1,41 @@
-import { StyleSheet, Text, View,SafeAreaView } from 'react-native'
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native'
 import React from 'react';
-import LogoGroup from '../../assets/LogoGroup.svg';
+import BigLogo from '../../assets/Logo.svg';
 
 export default function OnBoard() {
-  return (
-    <SafeAreaView style={styles.container}>
-      <LogoGroup style={styles.logo} width={179} />
-    </SafeAreaView>
-  )
+    return (
+        <SafeAreaView style={styles.container}>
+            <View style={styles.LogoGroup}>
+                <BigLogo style={styles.logo} />
+                <View style={styles.TextContainer}>
+                    <Text style={{ fontSize: 20, color: '#04B5DF',fontWeight:"200" }}>Your Guard,</Text>
+                    <Text style={{ fontSize: 20, color: '#000', textAlign: 'center',fontWeight:"200" }}>  On Grids </Text>
+                </View>
+            </View>
+        </SafeAreaView>
+    )
 }
 
 const styles = StyleSheet.create({
-    container:{
-        flex:1,
-        justifyContent:'center',
-        alignItems:'center',
-        backgroundColor:'#fff',
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#fff',
     },
-    logo:{
-        
-    }
+    LogoGroup: {
+        height: 250,
+        width: 250,
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    TextContainer: {
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "center",
+        marginTop:"5%"
+    },
 })
