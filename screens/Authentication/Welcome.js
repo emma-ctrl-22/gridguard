c07f9a82d0f8bd43612a8d8ee0c125dc335e8385
ellie-ctrl35 <146957037+ellie-ctrl35@users.noreply.github.com>
2024-05-22 19:18:01 +0100

    Refactor: Welcome screen done

--- screens/Authentication/Welcome.js
+++ screens/Authentication/Welcome.js
@@ -1,18 +1,50 @@
-import { StyleSheet, Text, View ,SafeAreaView} from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, Pressable, SafeAreaView, View } from 'react-native';
+import React from 'react';
+import Check from '../../assets/Check.svg';
+import SubmitButton from '../../components/SubmitButton';
 
 export default function Welcome() {
   return (
-    <SafeAreaView styles={styles.container}>
-      
+    <SafeAreaView style={styles.container}>
+      <View style={styles.content}>
+        <Check style={styles.logo} />
+        <Text style={styles.headerText}>Account Successfully Created</Text>
+        <Text style={styles.subText}>
+          Your account has been created successfully.{"\n"}Press continue to begin using the app
+        </Text>
+        <SubmitButton onPress='' text="Continue" width={335} height="7%" bR={5} bW={0} color="#fff" />
+        <Pressable style={{ marginTop: "4%" }}>
+          <Text style={{fontSize:13,color:"#c0c0c0",textAlign:"center"}}>
+            By continuing you agree to our{"\n"}Terms & Conditions
+          </Text>
+        </Pressable>
+      </View>
     </SafeAreaView>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
-    container:{
-        flex: 1,
-        alignItems: 'center',
-        backgroundColor: '#fff',
-    }
-})
+  container: {
+    flex: 1,
+   
+    alignItems: "center",
+  },
+  content: {
+    alignItems: "center",
+  },
+  logo: {
+    alignSelf: "center",
+    marginTop: "30%",
+  },
+  headerText: {
+    fontSize: 25,
+    fontWeight: "600",
+    marginTop: "25%",
+  },
+  subText: {
+    color: "#c0c0c0",
+    fontSize: 15,
+    textAlign: "center",
+    marginBottom:"60%"
+  },
+});
